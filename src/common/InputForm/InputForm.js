674b640dc771f00fbe5f3d850c570c196c3118cd
Shushko Oleg <shushko.oleg@gmail.com>
2020-12-08 20:42:58 +0200

    some fix

--- src/common/InputForm/InputForm.js
+++ src/common/InputForm/InputForm.js
@@ -43,18 +43,26 @@ const InputForm = ({ formType, inputName, onSubmit, isInvalidNumber, numberFormI
             hasError.current = !!meta.error;
 
             return (
-                <div className={ classes.form_container }>
-                    <div className={ classes.auth_user }>
-                        <textarea { ...input } { ...rest } className={ classes.auth_user_textarea } autoFocus={ true } />
-                        <button type="submit" className={ classes.auth_user_button }>Send</button>
-                        <div className={ classes.error_message }>
-                            { isInvalidNumber && !meta.modified && !meta.touched && <span>Invalid number</span> }
-                            { meta.error && meta.error !== 'Required field' && <span>{ meta.error }</span> }
-                            { meta.error === 'Required field' && meta.touched && !meta.active && <span>{ meta.error }</span> }
-                        </div>
+                <div className={ classes.auth_user }>
+                    <textarea { ...input } { ...rest } className={ classes.auth_user_textarea } autoFocus={ true } />
+                    <button type="submit" className={ classes.auth_user_button }>Send</button>
+                    <div className={ classes.error_message }>
+                        { isInvalidNumber && !meta.modified && !meta.touched && <span>Invalid number</span> }
+                        { meta.error && meta.error !== 'Required field' && <span>{ meta.error }</span> }
+                        { meta.error === 'Required field' && meta.touched && !meta.active && <span>{ meta.error }</span> }
                     </div>
                 </div>
             )
+        }
+    };
+
+    const onPressEnter = (event, form, pristine, handleSubmit) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            if (!pristine) {
+                handleSubmit();
+                (!hasError.current || formType === 'send_message_panel') && form.reset()
+            }
         }
     };
 
@@ -62,20 +70,12 @@ const InputForm = ({ formType, inputName, onSubmit, isInvalidNumber, numberFormI
         <Form onSubmit={ onSubmit }>
             { ({ handleSubmit, pristine, form }) => (
                 <form onSubmit={ handleSubmit }>
-                    <Field component={ Textarea } name={ inputName }
+                    <Field component={ Textarea }
+                           name={ inputName }
                            validate={ validators && composeValidators(...validators) }
-                           isInvalidNumber={ formType === 'auth_user' && isInvalidNumber }
-                           numberFormIsVisible={ formType === 'auth_user' && numberFormIsVisible }
-                           onKeyDown={ e => {
-                               if (e.key === 'Enter') {
-                                   e.preventDefault();
-                                   if (!pristine) {
-                                       handleSubmit();
-                                       if (!hasError.current) { form.reset() }
-                                       formType === 'send_message_panel' && form.reset()
-                                   }
-                               }
-                           } }
+                           isInvalidNumber={ isInvalidNumber }
+                           numberFormIsVisible={ numberFormIsVisible }
+                           onKeyDown={ event => onPressEnter(event, form, pristine, handleSubmit) }
                            placeholder={ placeholder }
                     />
                 </form>
